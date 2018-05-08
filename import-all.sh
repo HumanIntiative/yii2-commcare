25a16abf6d90cb81bd8a2963be369166d4618b53
Zein Miftah <zeinmiftah@gmail.com>
2018-05-08 09:04:06 +0700

    Commcare create command weekly / daily / hourly import

---
+++ import-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+nohup php yii import/quick & \
+nohup php yii import/berita & \
+nohup php yii import/cerita & \
+nohup php yii import/pm-detail & \
+nohup php yii import/pm-aggregate & \
+nohup php yii import/attachment
